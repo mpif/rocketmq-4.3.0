7850497b815c8a5bf9b0a63b2bade1195d5e70db
mpif <caiszf@163.com>
2019-08-12 15:41:02 +0800

    性能指标tps统计原理

--- common/src/main/java/org/apache/rocketmq/common/stats/StatsItem.java
+++ common/src/main/java/org/apache/rocketmq/common/stats/StatsItem.java
@@ -58,9 +58,24 @@ public class StatsItem {
             if (!csList.isEmpty()) {
                 CallSnapshot first = csList.getFirst();
                 CallSnapshot last = csList.getLast();
+                /**
+                 * value这里指消息数量,两个value相减,则是统计单位时间内的消息总量
+                 */
                 sum = last.getValue() - first.getValue();
+                /**
+                 * tps,是Transactions Per Second的缩写，也就是事务数/秒.
+                 * 而timestamp的单位是毫秒,所以这下面这句话等价于：
+                 * tps = (sum * 1.0d) / ((last.getTimestamp() - first.getTimestamp()) / 1000);
+                 *
+                 * sum是单位时间内的总数
+                 * (last.getTimestamp() - first.getTimestamp())计算的是时间总数(单位:秒)
+                 * 总数/时间总数=事务数/秒
+                 */
                 tps = (sum * 1000.0d) / (last.getTimestamp() - first.getTimestamp());
 
+                /**
+                 * 统计次数
+                 */
                 long timesDiff = last.getTimes() - first.getTimes();
                 if (timesDiff > 0) {
                     avgpt = (sum * 1.0d) / timesDiff;
@@ -93,6 +108,9 @@ public class StatsItem {
             @Override
             public void run() {
                 try {
+                    /**
+                     * 10秒钟采样1次
+                     */
                     samplingInSeconds();
                 } catch (Throwable ignored) {
                 }
@@ -103,6 +121,9 @@ public class StatsItem {
             @Override
             public void run() {
                 try {
+                    /**
+                     * 10分钟采样一次
+                     */
                     samplingInMinutes();
                 } catch (Throwable ignored) {
                 }
@@ -113,6 +134,9 @@ public class StatsItem {
             @Override
             public void run() {
                 try {
+                    /**
+                     * 1个小时采样一次
+                     */
                     samplingInHour();
                 } catch (Throwable ignored) {
                 }
@@ -152,9 +176,28 @@ public class StatsItem {
 
     public void samplingInSeconds() {
         synchronized (this.csListMinute) {
+            /**
+             * 插入链表尾部
+             */
             this.csListMinute.add(new CallSnapshot(System.currentTimeMillis(), this.times.get(), this.value
                 .get()));
+            /**
+             * 因为是10秒中采样一次,所以一分钟内有6个10秒;
+             * 又由于它是通过最后时间戳-开始时间戳的方式来统计时间的,所以需要多存一个。
+             * 比如统计2019-08-12 12:00:01到2019-08-12 12:01:01时间段的tps，经过了1分钟，每10秒采样一次，则csListminute记录的值有如下7个：
+             * 2019-08-12 12:00:01、
+             * 2019-08-12 12:00:11、
+             * 2019-08-12 12:00:21、
+             * 2019-08-12 12:00:31、
+             * 2019-08-12 12:00:41、
+             * 2019-08-12 12:00:51、
+             * 2019-08-12 12:01:01
+             * 所以只经过了1分钟、60秒钟的时间，但是却要记录7个时间值.
+             */
             if (this.csListMinute.size() > 7) {
+                /**
+                 * 删除链表头结点
+                 */
                 this.csListMinute.removeFirst();
             }
         }
@@ -164,6 +207,9 @@ public class StatsItem {
         synchronized (this.csListHour) {
             this.csListHour.add(new CallSnapshot(System.currentTimeMillis(), this.times.get(), this.value
                 .get()));
+            /**
+             * 10分钟采样一次,一个小时有6个10分钟,由于是通过结束时间减去开始时间的方式统计时间,所以需要记录7次。
+             */
             if (this.csListHour.size() > 7) {
                 this.csListHour.removeFirst();
             }
@@ -174,6 +220,9 @@ public class StatsItem {
         synchronized (this.csListDay) {
             this.csListDay.add(new CallSnapshot(System.currentTimeMillis(), this.times.get(), this.value
                 .get()));
+            /**
+             * 一个小时采样一次, 1天24个小时,由于是通过结束时间减去开始时间的方式统计时间,所以需要记录25次。
+             */
             if (this.csListDay.size() > 25) {
                 this.csListDay.removeFirst();
             }
