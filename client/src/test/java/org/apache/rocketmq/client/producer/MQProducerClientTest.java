28a734f79713ad5a7cc2fa86cb86ae891e91560a
mpif <caiszf@163.com>
2019-10-21 10:03:20 +0800

    添加注释

--- client/src/test/java/org/apache/rocketmq/client/producer/MQProducerClientTest.java
+++ client/src/test/java/org/apache/rocketmq/client/producer/MQProducerClientTest.java
@@ -19,12 +19,20 @@ public class MQProducerClientTest {
         String topic = "myTopicTest";
         /**
          * TODO 设置生产者组有什么用？
+         * 1.标识一类producer
+         * 2.可以通过运维工具查询这个发送消息应用下有多个producer实例
+         * 3.发送分布式事务消息时，如果producer中途意外宕机，Broker会主动回调Producer Group内的任意一台机器来确认事务状态
          */
         DefaultMQProducer producer = new DefaultMQProducer(producerGroupTemp);
 //        producer.setNamesrvAddr("10.58.84.55:9876");
 //        producer.setNamesrvAddr("10.58.84.55:9876;10.58.87.142:9876");
         producer.setNamesrvAddr("10.58.196.127:9876");
 //        producer.setCompressMsgBodyOverHowmuch(16);
+        /**
+         * 启用发送延迟故障配置
+         */
+        producer.setSendLatencyFaultEnable(true);
+
 //        Message message = new Message(topic, "This is a very huge message9920!".getBytes());
         Message message = null;
 
